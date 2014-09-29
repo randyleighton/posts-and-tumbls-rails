ae1b5a15b51cc8c778ea0ca4cfd43a0502dd1a5d
Randy Leighton <student@epicodus.com>
2014-09-29 12:22:22 -0700

    controller posts complete and forms started for post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,11 +2,36 @@ class PostsController < ApplicationController
   before_filter :authenticate_user!, except: [:index, :show]
 
   def index
-
+    @posts = Post.all
   end
 
   def new
     @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+    if @post.valid?
+      flash[:notice] = "Success"
+      redirect_to posts_path
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @posts = Post.all
+    @post = Post.find(params[:id])
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body, :user_id)
   end
 
 end
