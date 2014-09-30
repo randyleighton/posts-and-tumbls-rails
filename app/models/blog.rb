c45e47e1bacf38abb8b1f41da80920b8213cf273
Randy Leighton <student@epicodus.com>
2014-09-30 12:16:52 -0700

    blog.all showing body caption title

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,15 +1,17 @@
 class Blog
 
-  attr_reader :title
+  attr_reader :title, :caption, :body
 
   def initialize(params)
     @title = params["title"]
+    @caption = params["caption"]
+    @body = params["body"]
   end
 
   def self.all
     blogs = []
     client = Tumblr::Client.new
-    response = client.posts("randyleighton.tumblr.com")
+    response = client.posts("houseofsoundpdx.tumblr.com")
     response["posts"].each do |blog_attributes|
       blogs << Blog.new(blog_attributes)
     end
