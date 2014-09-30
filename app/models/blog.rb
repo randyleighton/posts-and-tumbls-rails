d9c7010314d7fdd009ba45c8033ab60ddcfcfb21
Randy Leighton <student@epicodus.com>
2014-09-30 15:50:31 -0700

    remove followers code partials

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -8,16 +8,22 @@ class Blog
     @body = params["body"]
     @type = params["type"]
     @image = params["photos"][0]["original_size"]["url"] if params["photos"]
+    @followers = params["follower"]
   end
 
   def self.all
     blogs = []
     client = Tumblr::Client.new
-    response = client.posts("houseofsoundpdx.tumblr.com")
+    response = client.posts("randyleighton.tumblr.com")
     response["posts"].each do |blog_attributes|
       blogs << Blog.new(blog_attributes)
     end
     blogs
   end
 
+  # def self.get_followers
+  #   followers = []
+  #   client = Tumblr::Client.new
+  #   response = client.followers("randyleighton.tumblr.com")
+  # end
 end
